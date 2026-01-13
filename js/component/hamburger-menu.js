50958b211dc18b916e9cd1075fb605a8dec2f736
sekiya-a <mingzhijiaguangu@gmail.com>
2026-01-13 19:52:41 +0900

    ハンバーガーメニューが完成しました。

--- js/component/hamburger-menu.js
+++ js/component/hamburger-menu.js
@@ -1,4 +1,57 @@
+import { initializeHeaderBackgroundToggle } from "./header-background-toggle.js";
+
 /**
  * ハンバーガーメニュー
  */
-export const initializeHamburgerMenu = () => {};
+export const initializeHamburgerMenu = () => {
+  const menu = document.querySelector(".js-header-menu");
+  const openButton = document.querySelector(".js-header-menu-open");
+  const closeButton = document.querySelector(".js-header-menu-close");
+
+  if (!openButton || !menu || !closeButton) return;
+
+  const openingKeyframes = { opacity: [0, 1] };
+  const closingKeyframes = { opacity: [1, 0] };
+  const options = { duration: 300, easing: "ease-out" };
+
+  let isAnimating = false;
+
+  const openMenu = () => {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    initializeHeaderBackgroundToggle(true);
+    menu.showModal();
+
+    const openingAnim = menu.animate(openingKeyframes, options);
+    openingAnim.onfinish = () => (isAnimating = false);
+  };
+
+  const closeMenu = () => {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    const closingAnim = menu.animate(closingKeyframes, options);
+    closingAnim.onfinish = () => {
+      menu.close();
+      initializeHeaderBackgroundToggle(false);
+      isAnimating = false;
+    };
+  };
+
+  openButton.addEventListener("click", openMenu);
+  closeButton.addEventListener("click", closeMenu);
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape" && menu.open) {
+      event.preventDefault();
+      closeMenu();
+    }
+  });
+
+  menu.addEventListener("click", (e) => {
+    if (e.target === menu) {
+      closeMenu();
+    }
+  });
+};
--- js/component/header-background-toggle.js
+++ js/component/header-background-toggle.js
@@ -1,4 +1,15 @@
 /**
  * ヘッダーの背景色変更
  */
-export const initializeHeaderBackgroundToggle = () => {};
+export const initializeHeaderBackgroundToggle = (isOpen) => {
+  const header = document.querySelector(".js-header");
+  if (!header) return;
+
+  if (isOpen) {
+    header.classList.add("is-open");
+    document.body.style.overflow = "hidden";
+  } else {
+    header.classList.remove("is-open");
+    document.body.style.overflow = "";
+  }
+};
